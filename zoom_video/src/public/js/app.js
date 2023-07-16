55fc54a625e3c4693151e84b71ef1c87f1e1bd63
kimdongwoo <siwol406@gmail.com>
2023-07-17 00:40:46 +0900

    화상채팅 구현

--- zoom_video/src/public/js/app.js
+++ zoom_video/src/public/js/app.js
@@ -14,6 +14,7 @@ let muted = false;
 let cameraOff = false;
 let roomName;
 let myPeerConnection;
+let myDataChannel;
 
 async function getCameras() {
   try {
@@ -87,8 +88,16 @@ function handleCameraClick() {
   }
 }
 
+// 카메라를 바꿔도 다른 브라우저에서 화면이 뜨도록 stream바꾸기
 async function handleCameraChange() {
   await getMedia(cameraSelect.value);
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind === 'video');
+    videoSender.replaceTrack(videoTrack);
+  }
 }
 
 muteBtn.addEventListener('click', handleMuteClick);
@@ -100,17 +109,18 @@ cameraSelect.addEventListener('input', handleCameraChange);
 const welcome = document.getElementById('welcome');
 const welcomeForm = welcome.querySelector('form');
 
-async function startMedia() {
+async function initCall() {
   welcome.hidden = true;
   call.hidden = false;
   await getMedia();
   makeConnection();
 }
 
-function handleWelcomeSubmit(event) {
+async function handleWelcomeSubmit(event) {
   event.preventDefault();
   const input = welcomeForm.querySelector('input');
-  socket.emit('join_room', input.value, startMedia);
+  await initCall();
+  socket.emit('join_room', input.value);
   roomName = input.value;
   input.value = '';
 }
@@ -119,21 +129,74 @@ welcomeForm.addEventListener('submit', handleWelcomeSubmit);
 
 // Socket Code
 
+// brave 브라우저에서 작동(먼저 방에 들어가는 브라우저)
 socket.on('welcome', async () => {
+  myDataChannel = myPeerConnection.createDataChannel('chat');
+  myDataChannel.addEventListener('message', (event) => console.log(event.data));
+  console.log('made data channel');
   const offer = await myPeerConnection.createOffer();
   myPeerConnection.setLocalDescription(offer);
   console.log('sent the offer');
   socket.emit('offer', offer, roomName);
 });
 
-socket.on('offer', (offer) => {
-  console.log(offer);
+// firefox에서 작동(뒤늦게 방에 들어가는 브라우저)
+socket.on('offer', async (offer) => {
+  myPeerConnection.addEventListener('datachannel', (event) => {
+    myDataChannel = event.channel;
+    myDataChannel.addEventListener('message', (event) =>
+      console.log(event.data)
+    );
+  });
+  console.log('received the offer');
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setLocalDescription(answer);
+  socket.emit('answer', answer, roomName);
+  console.log('sent the answer');
 });
+
+socket.on('answer', (answer) => {
+  console.log('received the answer');
+  myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on('ice', (ice) => {
+  console.log('received candidate');
+  myPeerConnection.addIceCandidate(ice);
+});
+
 // RTC Code
 
+// addStream 대신에 makeConnection을 쓴다!!
 function makeConnection() {
-  myPeerConnection = new RTCPeerConnection();
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          'stun:stun.l.google.com:19302',
+          'stun:stun1.l.google.com:19302',
+          'stun:stun2.l.google.com:19302',
+          'stun:stun3.l.google.com:19302',
+          'stun:stun4.l.google.com:19302',
+        ],
+      },
+    ],
+  });
+  myPeerConnection.addEventListener('icecandidate', handleIce);
+  myPeerConnection.addEventListener('addstream', handleAddStream);
   myStream
     .getTracks()
     .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
+
+// IceCandidate를 교환해 peer간 최적의 네트워크 경로 찾아내기
+function handleIce(data) {
+  console.log('sent candidate');
+  socket.emit('ice', data.candidate, roomName);
+}
+
+function handleAddStream(data) {
+  const peerFace = document.getElementById('peersStream');
+  peerFace.srcObject = data.stream;
 }

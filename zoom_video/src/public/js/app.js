db0d5fdaed985e69f077bb736b02b7bc7b0aecd7
김동우 <qwa406@naver.com>
2023-07-16 19:25:02 +0900

    uservideo화면 가져오기

---
+++ zoom_video/src/public/js/app.js
@@ -0,0 +1,16 @@
+const socket = io();
+
+const myFace = document.getElementById('myFace');
+
+let myStream;
+
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
